307958a4dc477dfe9a2a0de37d5a3e5b928abd7a
Tarxos <ftarxos@gmail.com>
2021-06-25 00:21:32 +0300

    fix problems

--- src/SubTasks/HomeWork1/Program.cs
+++ src/SubTasks/HomeWork1/Program.cs
@@ -6,25 +6,30 @@ using System.Net.Http;
 using System.Text.Json;
 using System.Threading.Tasks;
 using System.IO;
+using System.Collections.Concurrent;
+using System.Collections.Immutable;
 
 const string PathToFile = "result.json";
 const string ApiUrl = "https://jsonplaceholder.typicode.com";
 
-await WorkAsync(ApiUrl, PathToFile, true);
-
-
-static async Task WorkAsync(string apiUrl, string pathToFile, bool printResult)
+try
 {
-    PostsLoaderService.InitializeClient(apiUrl);
-    var posts = await PostsLoaderService.LoadPostsAsync(10);
+    PostsLoaderService.InitializeClient(ApiUrl);
+    var posts = await PostsLoaderService.LoadPostsAsync(4, 13);
 
-    await SaveToFileHandler.SaveToFileAsync(pathToFile, posts);
+    await SaveToFileHandler.SaveToFileAsync(PathToFile, posts.ToImmutableArray());
 
-    if (printResult)
+    Console.WriteLine("Posts loaded and saved to file result.txt");
+
+    if (false)
     {
         foreach (var post in posts)
             Console.WriteLine(post);
     }
+}
+catch (Exception e)
+{
+    Console.WriteLine(e.Message);
 }
 
 // Helpers
@@ -33,6 +38,7 @@ static class PostsLoaderService
 {
     private static HttpClient Client;
     private static readonly JsonSerializerOptions Options = new(JsonSerializerDefaults.Web);
+    private static readonly Lazy<ConcurrentBag<int>> FailLoad = new();
 
     public static void InitializeClient(string baseApiUrl)
     {
@@ -40,25 +46,56 @@ static class PostsLoaderService
         Client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
     }
 
-    public static async Task<IEnumerable<Post>> LoadPostsAsync(int count = 1)
+    public static async Task<IEnumerable<Post>> LoadPostsAsync(int from, int to)
     {
-        var tasks = Enumerable
-            .Range(1, count)
-            .Select(id => Client.GetStringAsync($"posts/{id}")
-            .ContinueWith(x => JsonSerializer.Deserialize<Post>(x.Result, Options)));
-        await Task.WhenAll(tasks);
-        return tasks.Select(x => x.Result);
+        if (from > to || from < 1 || to < 1)
+            throw new InvalidOperationException("From must be lower then To, and both must be greater than 1");
+        var toAwait = Enumerable.Range(from, to).Select(GetPost);
+        var tasks = Task.WhenAll(toAwait);
+        try
+        {
+            await tasks;
+        }
+        catch (Exception) { }
+
+        if(FailLoad.IsValueCreated && FailLoad.Value is { } bag && !bag.IsEmpty)
+        {
+            Console.WriteLine($"Fail to load posts: { string.Join(", ", bag.OrderBy(x=>x))}");
+        }
+
+        return toAwait.Where(x => !x.IsFaulted).Select(x => x.Result);
+    }
+    
+    private static async Task<Post> GetPost(int id)
+    {
+        try
+        {
+            if (id is 5 or 6) throw new InvalidOperationException("Fail");
+            var json = await Client.GetStringAsync($"posts/{id}");
+            return JsonSerializer.Deserialize<Post>(json, Options);
+        }
+        catch
+        {
+            FailLoad.Value.Add(id);
+            throw;
+        }
     }
 }
 
 static class SaveToFileHandler
 {
-    private static readonly JsonSerializerOptions Options = new() { WriteIndented = true };
-
-    public static async Task SaveToFileAsync<T>(string pathToFile, IEnumerable<T> data)
+    public static async Task SaveToFileAsync(string pathToFile, ImmutableArray<Post> data)
     {
-        using var stream = GetFileStream(pathToFile);
-        await JsonSerializer.SerializeAsync(stream, data, Options);
+        await using var stream = GetFileStream(pathToFile);
+        await using var writer = new StreamWriter(stream) { AutoFlush = true };
+        
+        for (int i = 0; i < data.Length; i++)
+        {
+            var item = data[i];
+            var content = $"{item.UserId}\r\n{item.Id}\r\n{item.Title}\r\n{item.Body}";
+            if(i == data.Length - 1) writer.Write($"{content}");
+            else writer.WriteLine($"{content}\r\n");
+        } 
     }
 
     private static Stream GetFileStream(string pathToFile)
