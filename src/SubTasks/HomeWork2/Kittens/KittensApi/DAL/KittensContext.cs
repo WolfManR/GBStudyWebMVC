328058b64929b2bb5c4824cc9e4b671a8ad7cab6
Tarxos <ftarxos@gmail.com>
2021-06-26 01:03:28 +0300

    kittens db context

---
+++ src/SubTasks/HomeWork2/Kittens/KittensApi/DAL/KittensContext.cs
@@ -0,0 +1,21 @@
+﻿using KittensApi.Models;
+
+using Microsoft.EntityFrameworkCore;
+
+using System.Reflection;
+
+namespace KittensApi.DAL
+{
+    public class KittensContext : DbContext
+    {
+        public KittensContext(DbContextOptions<KittensContext> options) : base(options) { }
+
+        public DbSet<Kitten> Kittens { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationForCurrentContext(Assembly.GetExecutingAssembly(), this);
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
