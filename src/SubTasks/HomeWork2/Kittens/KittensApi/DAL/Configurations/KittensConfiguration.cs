7df871798ad19099e53ee4e21bd452f386b7d916
Tarxos <ftarxos@gmail.com>
2021-06-26 01:04:06 +0300

    prototype kittens model configuration

---
+++ src/SubTasks/HomeWork2/Kittens/KittensApi/DAL/Configurations/KittensConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using KittensApi.Models;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace KittensApi.DAL.Configurations
+{
+    public class KittensConfiguration : IEntityTypeConfiguration<Kitten>
+    {
+        public void Configure(EntityTypeBuilder<Kitten> builder)
+        {
+            builder.Property(k => k.Nickname).IsRequired();
+        }
+    }
+}
