634b08d217ea77c2bf80bc13f1c685b60598cfdb
Tarxos <ftarxos@gmail.com>
2021-06-26 01:54:46 +0300

    prototype api with request and response model

--- src/SubTasks/HomeWork2/Kittens/KittensApi/Controllers/KittensStorageController.cs
+++ src/SubTasks/HomeWork2/Kittens/KittensApi/Controllers/KittensStorageController.cs
@@ -1,10 +1,27 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using KittensApi.Controllers.Requests;
+using KittensApi.Controllers.Responses;
+
+using Microsoft.AspNetCore.Mvc;
+
+using System;
+using System.Collections.Generic;
 
 namespace KittensApi.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("kittens")]
     [ApiController]
     public class KittensStorageController : ControllerBase
     {
+        [HttpPost]
+        public void Create(KittenCreateRequest request)
+        {
+
+        }
+
+        [HttpGet]
+        public IEnumerable<KittenGetResponse> Get()
+        {
+            return Array.Empty<KittenGetResponse>();
+        }
     }
 }
---
+++ src/SubTasks/HomeWork2/Kittens/KittensApi/Controllers/Requests/KittenCreateRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace KittensApi.Controllers.Requests
+{
+    public class KittenCreateRequest
+    {
+        public string Nickname { get; init; }
+        public double Weight { get; init; }
+        public string Color { get; init; }
+        public bool HasCertificate { get; init; }
+        public string Feed { get; init; }
+    }
+}
