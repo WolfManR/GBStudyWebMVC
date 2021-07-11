e3a26c68fc3534039318270811be1fb58f1f2ad0
Tarxos <ftarxos@gmail.com>
2021-07-12 01:00:09 +0300

    add DIContainer registration configuration for DataLayer

---
+++ src/SubTasks/HomeWork2/Kittens/DataBase/Registration.cs
@@ -0,0 +1,15 @@
+﻿using DataBase.Repository;
+using DataLayer.Abstractions.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DataBase
+{
+    public static class Registration
+    {
+        public static IServiceCollection AddDataLayer(this IServiceCollection services)
+        {
+            services.AddTransient<IKittensRepository, KittensRepository>();
+            return services;
+        }
+    }
+}
