6d87683c88eb978d102a0e16a2dbede5eab04868
Tarxos <ftarxos@gmail.com>
2021-07-13 02:09:50 +0300

    add crud behavior to kitten controller, create blank for filtering actions

--- src/SubTasks/HomeWork2/Kittens/KittensApi/Controllers/KittensStorageController.cs
+++ src/SubTasks/HomeWork2/Kittens/KittensApi/Controllers/KittensStorageController.cs
@@ -30,11 +30,41 @@ namespace KittensApi.Controllers
             await _kittensService.Add(_mapper.Map<Kitten>(request));
         }
 
+        [HttpPut]
+        public async Task UpdateAsync([FromBody] KittenUpdateRequest request)
+        {
+            await _kittensService.Update(_mapper.Map<Kitten>(request));
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task DeleteAsync([FromRoute] int id)
+        {
+            await _kittensService.Delete(id);
+        }
+
         [HttpGet]
         public async Task<IEnumerable<KittenGetResponse>> GetAsync()
         {
             var data = await _kittensService.Get();
             return _mapper.Map<List<KittenGetResponse>>(data);
+        }
+
+        [HttpGet("{size:int}/{page:int}")]
+        public async Task GetPageAsync([FromRoute] int size, [FromRoute] int page)
+        {
+
+        }
+
+        [HttpGet]
+        public async Task GetWithSearchAsync([FromQuery] string search)
+        {
+
+        }
+
+        [HttpGet("{size:int}/{page:int}")]
+        public async Task GetPageWithSearchAsync([FromRoute] int size, [FromRoute] int page, [FromQuery] string search)
+        {
+
         }
     }
 }
