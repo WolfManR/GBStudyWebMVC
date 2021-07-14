6173b4726a62fb2240272d10669043ddfce33360
Tarxos <ftarxos@gmail.com>
2021-07-15 02:09:30 +0300

    add IEntity interface and Veterinary Clinic

---
+++ src/SubTasks/HomeWork2/Kittens/DataBase.Abstractions/Entities/VeterinaryClinic.cs
@@ -0,0 +1,10 @@
+﻿namespace DataBase.Abstractions.Entities
+{
+    public class VeterinaryClinic : IEntity<int>
+    {
+        public int Id { get; init; }
+
+        public string Name { get; set; }
+        public string[] HealsAnimals { get; set; }
+    }
+}
