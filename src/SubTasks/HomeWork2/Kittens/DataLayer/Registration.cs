a541a414c78367f530732315a4fdc00c37364647
Tarxos <ftarxos@gmail.com>
2021-07-17 01:27:43 +0300

    add ClinicMaps, update DataLayer registration in DI Container

--- src/SubTasks/HomeWork2/Kittens/DataLayer/Registration.cs
+++ src/SubTasks/HomeWork2/Kittens/DataLayer/Registration.cs
@@ -12,7 +12,8 @@ namespace DataLayer
         public static IServiceCollection AddDataLayer(this IServiceCollection services)
         {
             services.AddTransient<IKittensRepository, KittensRepository>();
-            TypeAdapterConfig.GlobalSettings.Apply(new KittensMaps());
+            services.AddTransient<IClinicsRepository, ClinicsRepository>();
+            TypeAdapterConfig.GlobalSettings.Apply(new KittensMaps(), new ClinicMaps());
             return services;
         }
     }
