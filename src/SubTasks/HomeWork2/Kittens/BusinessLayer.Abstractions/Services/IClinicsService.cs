e5f9a4eff2bbd850b02f1e512c6e83af8238e52d
Tarxos <ftarxos@gmail.com>
2021-07-17 23:49:07 +0300

    prototype of Clinic Service

---
+++ src/SubTasks/HomeWork2/Kittens/BusinessLayer.Abstractions/Services/IClinicsService.cs
@@ -0,0 +1,9 @@
+﻿using BusinessLayer.Abstractions.Models;
+
+namespace BusinessLayer.Abstractions.Services
+{
+    public interface IClinicsService : IService<Clinic, int>
+    {
+        
+    }
+}
