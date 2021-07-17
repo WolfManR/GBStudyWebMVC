dc5233809acc66e2d18fc64140edb14ee3e82492
Tarxos <ftarxos@gmail.com>
2021-07-18 01:02:55 +0300

    add missed behavior for clinics service interface

--- src/SubTasks/HomeWork2/Kittens/BusinessLayer.Abstractions/Services/IClinicsService.cs
+++ src/SubTasks/HomeWork2/Kittens/BusinessLayer.Abstractions/Services/IClinicsService.cs
@@ -1,9 +1,14 @@
 ﻿using BusinessLayer.Abstractions.Models;
 
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
 namespace BusinessLayer.Abstractions.Services
 {
     public interface IClinicsService : IService<Clinic, int>
     {
-        
+        Task<IEnumerable<Kitten>> ListKittensInClinic(int clinicId);
+        Task RegisterKittenInClinic(int clinicId, int kittenId);
+        Task UnRegisterKittenFromClinic(int clinicId, int kittenId);
     }
 }
