bd6e37966c85af3f68824825cf3e4c5e3c00849b
Tarxos <ftarxos@gmail.com>
2021-07-19 00:36:08 +0300

    Authorization DataBase layers blanks

---
+++ src/SubTasks/HomeWork2/Kittens/Authorization/Authorization.DataBase.Abstractions/Roles.cs
@@ -0,0 +1,8 @@
+﻿namespace Authorization.DataBase.Abstractions
+{
+    public static class Roles
+    {
+        public const string Admin = "Admin";
+        public const string User = "User";
+    }
+}
