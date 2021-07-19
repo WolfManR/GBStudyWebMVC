2ce7bc24799408f12abd268d2f19e8ffe1327683
Tarxos <ftarxos@gmail.com>
2021-07-19 23:16:10 +0300

    fix paging query extension

--- src/SubTasks/HomeWork2/Kittens/DataLayer/Filters/QueryExtensions.cs
+++ src/SubTasks/HomeWork2/Kittens/DataLayer/Filters/QueryExtensions.cs
@@ -7,7 +7,7 @@ namespace DataLayer.Filters
     {
         public static IQueryable<T> GetPage<T>(this IQueryable<T> query, PageFilter filter)
         {
-            return query.Skip(filter.Page * filter.Size).Take(filter.Size);
+            return query.Skip((filter.Page > 1 ? filter.Page - 1 : 0) * filter.Size).Take(filter.Size);
         }
 
         public static IQueryable<T> SearchWith<T, TData>(this IQueryable<T> query, SearchFilter<T, TData> filter) where TData : SearchFilterData
