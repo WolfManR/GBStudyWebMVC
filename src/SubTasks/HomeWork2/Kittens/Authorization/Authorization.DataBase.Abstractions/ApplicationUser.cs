24425f96a42edd2654c686673d5390d20430e64c
Tarxos <ftarxos@gmail.com>
2021-07-21 00:49:42 +0300

    add Ed Identity db context and extend it identity user to have in model latest refresh token for user

---
+++ src/SubTasks/HomeWork2/Kittens/Authorization/Authorization.DataBase.Abstractions/ApplicationUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Authorization.DataBase.Abstractions
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public RefreshToken LatestRefreshToken { get; set; }
+    }
+}
