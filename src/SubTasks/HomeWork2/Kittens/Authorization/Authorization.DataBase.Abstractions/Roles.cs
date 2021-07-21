ca2e871da256c550f7bf4c4323abc018c87b4f6e
Tarxos <ftarxos@gmail.com>
2021-07-22 01:18:22 +0300

    remove Roles from authorization database abstrations

--- src/SubTasks/HomeWork2/Kittens/Authorization/Authorization.DataBase.Abstractions/Roles.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Authorization.DataBase.Abstractions
-{
-    public static class Roles
-    {
-        public const string Admin = "Admin";
-        public const string User = "User";
-    }
-}
