75c04c6d7a27184017117ed28701750305d9dd31
Tarxos <ftarxos@gmail.com>
2021-08-02 01:32:28 +0300

    add AnalysisService to BusinessLayer

---
+++ src/SubTasks/HomeWork2/Kittens/BusinessLayer.Abstractions/Services/IAnalysisService.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+using BusinessLayer.Abstractions.Models.Analyzes;
+
+namespace BusinessLayer.Abstractions.Services
+{
+    public interface IAnalysisService
+    {
+        Task<int> AssignAnalysisToKitten(InspectionAnalysis analysis, int clinicId, int kittenId);
+        Task<int> AssignAnalysisToKitten(BloodAnalysis analysis, int clinicId, int kittenId);
+    }
+}
