6d7675047af423ad7d11ce5c4bf5f9cf06e954f4
Tarxos <ftarxos@gmail.com>
2021-08-06 01:32:07 +0300

    added a behavior preventing further growth of the fetch request if no parameter was passed

--- src/SubTasks/HomeWork2/Kittens/DataLayer/Filters/QueryExtensions.cs
+++ src/SubTasks/HomeWork2/Kittens/DataLayer/Filters/QueryExtensions.cs
@@ -7,11 +7,13 @@ namespace DataLayer.Filters
     {
         public static IQueryable<T> GetPage<T>(this IQueryable<T> query, PageFilter filter)
         {
+            if (filter is null) return query.Take(0);
             return query.Skip((filter.Page > 1 ? filter.Page - 1 : 0) * filter.PageSize).Take(filter.PageSize);
         }
 
         public static IQueryable<T> SearchWith<T, TData>(this IQueryable<T> query, SearchFilter<T, TData> filter) where TData : SearchFilterData
         {
+            if (filter is null) return query.Take(0);
             return query.Where(filter.Inject());
         }
     }
