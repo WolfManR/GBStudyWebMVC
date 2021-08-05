a1a731a819158661e85014e4d02a949301783a9b
Tarxos <ftarxos@gmail.com>
2021-08-06 02:35:57 +0300

    add Tests for BusinessLayer for clinic and kitten validations

---
+++ src/SubTasks/HomeWork2/Kittens/Tests/BusinessLayerTests/ClinicValidationTests.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Linq;
+using BusinessLayer.Abstractions.Models;
+using BusinessLayer.Abstractions.Validations;
+using BusinessLayer.Validations;
+using Xunit;
+
+namespace BusinessLayerTests
+{
+    public class ClinicValidationTests
+    {
+        private static readonly ValidationService<Clinic> ValidationService = new ClinicValidation();
+
+        [Fact]
+        public void ClinicName_CannotBeEmpty()
+        {
+            var toTest = new Clinic
+            {
+                Name = ""
+            };
+
+            var actual = ValidationService.ValidateEntry(toTest)[0];
+
+            Assert.Equal("BCL-41", actual.Code);
+            Assert.Equal(nameof(Clinic.Name), actual.PropertyName);
+        }
+
+        [Fact]
+        public void ClinicName_CannotBeNULL()
+        {
+            var toTest = new Clinic
+            {
+                Name = null
+            };
+
+            var actual = ValidationService.ValidateEntry(toTest)[0];
+
+            Assert.Equal("BCL-41", actual.Code);
+            Assert.Equal(nameof(Clinic.Name), actual.PropertyName);
+        }
+
+        [Fact]
+        public void ClinicNameNotNULLOrEmpty_IsCorrect()
+        {
+            var toTest = new Clinic
+            {
+                Name = "Lorem"
+            };
+
+            var actual = ValidationService.ValidateEntry(toTest).Count;
+
+            Assert.Equal(0, actual);
+        }
+    }
+}
